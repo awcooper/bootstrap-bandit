fdd01b553137f7b6c4338f5b637bf91f1f6a4f47
awcooper <wesner.cooper@gmail.com>
2018-07-04 23:04:37 -0400

    figured out youtube api :joy:, Added random video choice to home page

--- app/controllers/metube_controller.rb
+++ app/controllers/metube_controller.rb
@@ -1,33 +1,17 @@
 class MetubeController < ApplicationController
-  require 'google/api_client'
+  require 'google/apis/youtube_v3'
 
-  DEVELOPER_KEY = 'AIzaSyBMTnPfDg1XfUKviYCDsGontA8VC'
-
-  YOUTUBE_API_SERVICE_NAME = 'youtube'
-  YOUTUBE_API_VERSION = 'v3'
+  DEVELOPER_KEY = ENV.fetch("GOOGLE_DEVELOPER_KEY")
 
   def get_service
-    client = Google::APIClient.new(
-      :key => DEVELOPER_KEY,
-      :authorization => nil,
-      :application_name => "metube",
-      :application_version => '1.0.0'
-    )
-    youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
-
-    return client, youtube
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = DEVELOPER_KEY
+    service
   end
 
   def metube
-    client, youtube = get_service
-    @some_videos = client.execute!(
-        :api_method => youtube.search.list,
-        :parameters => {
-            :part => 'snippet',
-            :q => 'Google',
-            :maxResults => 25
-        }
-    )
-
+    service = get_service
+    @result_videos = service.list_videos("snippet", chart: "mostPopular")
+    @player = service.list_videos("player", chart: "mostPopular", max_results: 50, ).items.sample
   end
 end
