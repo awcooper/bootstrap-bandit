db1911baf56e6bc3ee8f0e1160a75cc581efc538
awcooper <wesner.cooper@gmail.com>
2018-08-17 15:38:31 -0400

    changed database to pg for heroku app

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180606024921) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "eddit_posts", force: :cascade do |t|
     t.integer "score"
     t.string "username"
