7790c87d8125f1ff5d4ff8c5d203e4e1b0a47040
awcooper <wesner.cooper@gmail.com>
2018-08-26 16:50:59 -0400

    add h2 to card

--- app/controllers/webfliks_controller.rb
+++ app/controllers/webfliks_controller.rb
@@ -1,6 +1,8 @@
 class WebfliksController < ApplicationController
 
+
   def webfliks
+    @image_keywords = ['movie', 'princess', 'comedy' 'archer', 'fight', 'action', 'drama', 'romcom', 'advenure','badass','warrior','dragon']
   end
 
 end
